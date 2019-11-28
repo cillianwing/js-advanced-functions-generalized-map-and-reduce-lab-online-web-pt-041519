6510d8f69438d0792b1082815dabc12fc14a7f5e
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-28 18:28:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Add your functions here
 function map(sourceArray, sourceFunction) {
   let arr = [];
-  
+  for (let i = 0; i < sourceArray.length; i++) {
+    arr.push(sourceFunction(sourceArray[i]));
+  }
+  return arr;
 }
