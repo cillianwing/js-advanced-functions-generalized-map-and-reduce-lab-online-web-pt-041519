6efce7ecd663b541626f7c73b4390b1927bb4930
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-28 18:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function map(sourceArray, sourceFunction) {
   return arr;
 }
 
-function reduce(sourceArray, sourceFunction, startingValue=[]) {
+function reduce(sourceArray, sourceFunction, startingValue=0) {
   let total = startingValue;
   for (let i = 0; i < sourceArray.length; i++) {
     total = sourceFunction(sourceArray[i], total)
